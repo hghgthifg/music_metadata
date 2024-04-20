aa56c384fd90c9a23299b6a584a2a9ac52532d60
hghgthifg <hghgthifg@outlook.com>
2024-04-20 23:53:12 +0800

    feat: full support for id3v1 tag

--- lib/src/id3v1/id3v1_parser.dart
+++ lib/src/id3v1/id3v1_parser.dart
@@ -4,8 +4,8 @@ import "../metadata.dart";
 import "../parser.dart";
 
 extension StringTagEditExtensions on String {
-  String trimTag() {
-    return replaceAll(new RegExp(r'\0+$'), '');
+  String? trimTag() {
+    return replaceAll(RegExp(r'\0+$'), '');
   }
 }
 
@@ -42,19 +42,231 @@ class ID3v1Parser extends Parser {
     var album = String.fromCharCodes(buffer.sublist(63, 93));
     metadata.album = album.trimTag();
 
+    // get the year
     var year = String.fromCharCodes(buffer.sublist(93, 97));
-    metadata.year = int.tryParse(year.trimTag());
+    metadata.year =
+        (year.trimTag() != null) ? int.tryParse(year.trimTag()!.trim()) : null;
 
-    var comment = String.fromCharCodes(buffer.sublist(97, 127));
-    metadata.comment = comment.trimTag();
-
-    // get the track number
-    var track = buffer[126];
+    // get the comment and the track number
+    // if type is 0, meaning the comment is 30 characters long
+    // if type is 1, meaning the comment is 28 characters long, and the track number is in the 126th byte
+    String? comment;
+    int? track;
+    var type = buffer[125];
+    if (type == 1) {
+      comment = String.fromCharCodes(buffer.sublist(97, 127));
+      track = null;
+    } else if (type == 0) {
+      comment = String.fromCharCodes(buffer.sublist(97, 125));
+      track = buffer[126];
+    }
+    metadata.comment = comment?.trimTag();
     metadata.track = track;
 
     // get the genre
+    var genreCode = buffer[127];
+    if (genreCode != 255) {
+      metadata.genre = <String>[];
+      metadata.genre?.add(genres[genreCode]);
+    }
+
     // TODO implement genre parsing
 
     return metadata;
   }
+
+  static const genres = [
+    'Blues',
+    'Classic Rock',
+    'Country',
+    'Dance',
+    'Disco',
+    'Funk',
+    'Grunge',
+    'Hip-Hop',
+    'Jazz',
+    'Metal',
+    'New Age',
+    'Oldies',
+    'Other',
+    'Pop',
+    'R&B',
+    'Rap',
+    'Reggae',
+    'Rock',
+    'Techno',
+    'Industrial',
+    'Alternative',
+    'Ska',
+    'Death Metal',
+    'Pranks',
+    'Soundtrack',
+    'Euro-Techno',
+    'Ambient',
+    'Trip-Hop',
+    'Vocal',
+    'Jazz+Funk',
+    'Fusion',
+    'Trance',
+    'Classical',
+    'Instrumental',
+    'Acid',
+    'House',
+    'Game',
+    'Sound Clip',
+    'Gospel',
+    'Noise',
+    'Alt. Rock',
+    'Bass',
+    'Soul',
+    'Punk',
+    'Space',
+    'Meditative',
+    'Instrumental Pop',
+    'Instrumental Rock',
+    'Ethnic',
+    'Gothic',
+    'Darkwave',
+    'Techno-Industrial',
+    'Electronic',
+    'Pop-Folk',
+    'Eurodance',
+    'Dream',
+    'Southern Rock',
+    'Comedy',
+    'Cult',
+    'Gangsta Rap',
+    'Top 40',
+    'Christian Rap',
+    'Pop/Funk',
+    'Jungle',
+    'Native American',
+    'Cabaret',
+    'New Wave',
+    'Psychedelic',
+    'Rave',
+    'Showtunes',
+    'Trailer',
+    'Lo-Fi',
+    'Tribal',
+    'Acid Punk',
+    'Acid Jazz',
+    'Polka',
+    'Retro',
+    'Musical',
+    'Rock & Roll',
+    'Hard Rock',
+    'Folk',
+    'Folk/Rock',
+    'National Folk',
+    'Swing',
+    'Fast-Fusion',
+    'Bebob',
+    'Latin',
+    'Revival',
+    'Celtic',
+    'Bluegrass',
+    'Avantgarde',
+    'Gothic Rock',
+    'Progressive Rock',
+    'Psychedelic Rock',
+    'Symphonic Rock',
+    'Slow Rock',
+    'Big Band',
+    'Chorus',
+    'Easy Listening',
+    'Acoustic',
+    'Humour',
+    'Speech',
+    'Chanson',
+    'Opera',
+    'Chamber Music',
+    'Sonata',
+    'Symphony',
+    'Booty Bass',
+    'Primus',
+    'Porn Groove',
+    'Satire',
+    'Slow Jam',
+    'Club',
+    'Tango',
+    'Samba',
+    'Folklore',
+    'Ballad',
+    'Power Ballad',
+    'Rhythmic Soul',
+    'Freestyle',
+    'Duet',
+    'Punk Rock',
+    'Drum Solo',
+    'A Cappella',
+    'Euro-House',
+    'Dance Hall',
+    'Goa',
+    'Drum & Bass',
+    'Club-House',
+    'Hardcore',
+    'Terror',
+    'Indie',
+    'BritPop',
+    'Negerpunk',
+    'Polsk Punk',
+    'Beat',
+    'Christian Gangsta Rap',
+    'Heavy Metal',
+    'Black Metal',
+    'Crossover',
+    'Contemporary Christian',
+    'Christian Rock',
+    'Merengue',
+    'Salsa',
+    'Thrash Metal',
+    'Anime',
+    'JPop',
+    'Synthpop',
+    'Abstract',
+    'Art Rock',
+    'Baroque',
+    'Bhangra',
+    'Big Beat',
+    'Breakbeat',
+    'Chillout',
+    'Downtempo',
+    'Dub',
+    'EBM',
+    'Eclectic',
+    'Electro',
+    'Electroclash',
+    'Emo',
+    'Experimental',
+    'Garage',
+    'Global',
+    'IDM',
+    'Illbient',
+    'Industro-Goth',
+    'Jam Band',
+    'Krautrock',
+    'Leftfield',
+    'Lounge',
+    'Math Rock',
+    'New Romantic',
+    'Nu-Breakz',
+    'Post-Punk',
+    'Post-Rock',
+    'Psytrance',
+    'Shoegaze',
+    'Space Rock',
+    'Trop Rock',
+    'World Music',
+    'Neoclassical',
+    'Audiobook',
+    'Audio Theatre',
+    'Neue Deutsche Welle',
+    'Podcast',
+    'Indie Rock',
+    'G-Funk',
+    'Dubstep',
+    'Garage Rock',
+    'Psybient'
+  ];
 }
--- test/music_metadata_test.dart
+++ test/music_metadata_test.dart
@@ -7,13 +7,15 @@ void main() {
       // Additional setup goes here.
     });
 
-    test('First Test', () {
+    test('First Test: Id3v1 tags in mp3 file', () {
       final parser = loadFromFile("test/sample/Piano Magic Motive.mp3");
       final metadata = parser.parse();
-
-      print(metadata.title);
-
       expect(metadata.title, "Piano Magic Motive");
+      expect(metadata.artist, "Kevin MacLeod");
+      expect(metadata.album, "Royalty Free");
+      expect(metadata.comment, "Test Case 1");
+      expect(metadata.track, 1);
+      expect(metadata.genre![0], "Electronic");
     });
   });
 }
